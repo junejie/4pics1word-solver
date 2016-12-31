359ac58ea4984d72c20b3e7881526ba0ccd4164f
Junejie Ruzol <junejie.ruzol@zumtobelgroup.com>
2016-12-31 12:50:34 +0800

    with web support

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Run
     $python main.py $words $numberOfChars
     
     $python main.py sgdddfre 4
+
+
+Accessible in browser
+
+    http://localhost:8082
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import cgi
 import sys
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
+PORT_NUMBER = 8082
 expected_len = int(sys.argv[2])
 valid_letters = sys.argv[1]
 words = []
 words_step_2 = []
 valid_letters_array = []
 words_with_multi_letter = []
+
 for v in valid_letters:
     valid_letters_array.append(v)
 
@@ -71,3 +75,53 @@ with open('words.txt') as file:
     for words in result:
         print words
     print '--'*50
+
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(BaseHTTPRequestHandler):
+    
+    #Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("<form method='post' action='/'>")
+        self.wfile.write("<input name='word' type='text'>")
+        self.wfile.write("<input name='count' type='text'>")
+        self.wfile.write("<input type='submit' value='Find'>")
+        self.wfile.write("</form>")
+        return
+
+    #Handler for the POST requests
+    def do_POST(self):
+        if self.path=="/":
+
+            form = cgi.FieldStorage(
+                fp=self.rfile, 
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                         'CONTENT_TYPE':self.headers['Content-Type'],
+            })
+            print 'form:',form
+            print "Your name is: %s" % form["word"].value
+            print "Your name is: %s" % form["count"].value
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write("Thanks %s !" % form["word"].value)
+            return      
+
+try:
+    #Create a web server and define the handler to manage the
+    #incoming request
+    server = HTTPServer(('', PORT_NUMBER), myHandler)
+    print 'Started httpserver on port ' , PORT_NUMBER
+    
+    #Wait forever for incoming htto requests
+    server.serve_forever()
+
+except KeyboardInterrupt:
+    print '^C received, shutting down the web server'
+    server.socket.close()
+    
